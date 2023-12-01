f92f4496ea9dd6395ea6539dbbd5594b68227d4b
AidaZZ <128605556+AidaZZ@users.noreply.github.com>
2023-12-01 11:14:58 +0100

    update

--- Pong/src/CountDown.java
+++ Pong/src/CountDown.java
@@ -5,21 +5,22 @@ import java.awt.Graphics;
 public class CountDown {
     int secondsLeft;
     long roundStartTime;
-    final int COUNTDOWN_DURATION;
+    final int COUNTDOWN_DURATION; // Add this constant
 
     CountDown(int minutes) {
         this.secondsLeft = minutes * 60;
-        this.COUNTDOWN_DURATION = minutes * 60;
+        this.COUNTDOWN_DURATION = minutes * 60; // Initialize the constant
         this.roundStartTime = System.currentTimeMillis();
     }
 
     public void update() {
         long currentTime = System.currentTimeMillis();
         long elapsedTime = currentTime - roundStartTime;
-        secondsLeft = (int) Math.max(0, (COUNTDOWN_DURATION * 1000 - elapsedTime) / 1000);
+        secondsLeft = (int) Math.max(0, (COUNTDOWN_DURATION * 1000 - elapsedTime) / 1000);//Calculates the remaining seconds by subtracting the elapsed time from the total duration.
+        // The result is divided by 1000 to convert from milliseconds to seconds. The Math.max(0, ...) ensures that the remaining seconds do not go below 0.
 
         if (secondsLeft <= 0) {
-            secondsLeft = 0;
+            secondsLeft = 0; // Set to 0 when the time is up.Sets secondsLeft to 0 if it becomes negative. This step is an additional safety check to ensure that the countdown does not show negative value
         }
     }
 
